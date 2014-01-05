1468181c5f022ad48ab465eaaaf9d58a36aacb62
John Pritchard <jdp@syntelos.org>
2014-01-05 14:26:44 -0500

    change copyright statement

--- data/src/vector/data/DataService.java
+++ data/src/vector/data/DataService.java
@@ -1,6 +1,6 @@
 /*
  * Vector (http://code.google.com/p/java-vector/)
- * Copyright (C) 2013, The DigiVac Company
+ * Copyright (C) 2013, John Pritchard, Syntelos
  * 
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
